8856b618dbe2c79df57235979fb2a9ddb58ef964
Laura <40493346+Laurapaws@users.noreply.github.com>
2022-12-19 19:45:42 +0000

    Updated JSON and added some stats

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import random
 import json
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import Counter
 
-SEED = "11111!"
+SEED = "cseff324"
 WEATHER_DATA = "weather.json"
 
 dice = random.Random(SEED)
 weather_data = json.load(open(WEATHER_DATA))
 
 class Weather():
-    def __init__(self, name=None, effects=None, calming1=None, worsening1=None, worsening2=None, duration=None, roll = None):
+    def __init__(self, name=None, effects=None, calming1=None, calming2=None, worsening1=None, worsening2=None, duration=None, roll=None):
         self.name = name
         self.effects = effects
         self.calming1 = calming1
+        self.calming2 = calming2
         self.worsening1 = worsening1
         self.worsening2 = worsening2
         self.duration = duration
@@ -21,6 +25,7 @@ def populate_weather_object(current_weather_object, previous_weather_roll):
     current_weather_object.name = weather_data["weather"][str(previous_weather_roll)]["name"]
     current_weather_object.effects = weather_data["weather"][str(previous_weather_roll)]["effects"]
     current_weather_object.calming1 = weather_data["weather"][str(previous_weather_roll)]["calming1"]
+    current_weather_object.calming2 = weather_data["weather"][str(previous_weather_roll)]["calming2"]
     current_weather_object.worsening1 = weather_data["weather"][str(previous_weather_roll)]["worsening1"]
     current_weather_object.worsening2 = weather_data["weather"][str(previous_weather_roll)]["worsening2"]
     current_weather_object.roll = weather_data["weather"][str(previous_weather_roll)]["roll"]
@@ -50,20 +55,16 @@ def weather_status():
     
     weather_status = "Error weather not calculated"
     roll_result = roll(1,100)
-    print(f"Status roll result = {str(roll_result)}")
     
-    if roll_result <= 50:
+    if roll_result <= 25:
         weather_status = "calming1"
-        print("Getting Better...")
-        print("")
+    if roll_result <= 50:
+        weather_status = "calming2"
     elif roll_result <=75:
         weather_status = "worsening1"
-        print("Getting Worse...")
-        print("")
     elif roll_result <= 100:
         weather_status = "worsening2"
-        print("Getting Even Worse...")
-        print("")
+
         
     return weather_status
 
@@ -81,7 +82,9 @@ def next_weather(weather):
 def print_weather(weather):
     print(f"Weather: {weather.name}")
     print(f"Duration: {str(weather.duration)} hours")
-    print(f"Effects: {str(weather.effects)}")
+    print(f"Effects: ")
+    for effect in weather.effects:
+        print(effect)
 
 def weather_sample():
     current_weather = long_rest()
@@ -95,12 +98,37 @@ def weather_sample():
     even_newer = next_weather(new_weather)
     print_weather(even_newer)
 
+weather_list = []
+
+# weather_now = long_rest()
+
+weather_sample()
+
+# runs = 100000
+# print(f"{runs} iterations")
+
+# for x in range(runs):
+#     #print_weather(weather_now)
+#     #print("--------------------------")
+    
+#     weather_list.append(weather_now.name)
+    
+#     weather_now = next_weather(weather_now)
 
 
-weather_now = long_rest()
+# c = Counter(weather_list)
+# a = c.keys() 
+# b = c.values() 
+# for dict_item in c:
+#     stat = 100 * (c[dict_item] / runs)
+#     stat = round(stat, 1)
+#     print(f"{dict_item}: {stat}%")
 
-for x in range(10):
-    print_weather(weather_now)
-    print("--------------------------")
-    weather_now = next_weather(weather_now)
+
+# weather_list_unique = list(set(weather_list))
+# counts = [weather_list.count(value) for value in weather_list_unique]
+# barcontainer = plt.bar(range(len(weather_list_unique)),counts)
+# plt.bar_label(barcontainer,weather_list_unique, label_type='edge')
+# plt.axis('off')
+# plt.show()
 
